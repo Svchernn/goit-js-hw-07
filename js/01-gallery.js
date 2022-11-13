0389f23fb66f965a1c0820706067af132f2f8d8d
Svitlana Cherniakova <107955029+Svchernn@users.noreply.github.com>
2022-11-13 23:33:35 +0200

    hw-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const GalleryRef = document.querySelector(".gallery");
+const createImgItem = ({ preview, original, description }) => {
+  const imgItem = `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+      </a>
+</div>`;
+  return imgItem;
+};
+const galleryImg = galleryItems.map(createImgItem).join("");
+
+GalleryRef.insertAdjacentHTML("afterbegin", galleryImg);
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target === event.CurrentTarget) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src= ${event.target.dataset.source}>
+`);
+
+  instance.show();
+
+  document.body.addEventListener("keydown", closeModal);
+
+  function closeModal(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      document.body.removeEventListener("keydown", closeModal);
+    }
+  }
+}
+
+GalleryRef.addEventListener("click", onClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const GalleryRef = document.querySelector(".gallery");
+const createImgItem = ({ preview, original, description }) => {
+  const imgItem = `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  return imgItem;
+};
+const galleryImg = galleryItems.map(createImgItem).join("");
+GalleryRef.insertAdjacentHTML("afterbegin", galleryImg);
 
-console.log(galleryItems);
+// console.log(galleryItems);
+let newGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  animationSlide: true,
+});
